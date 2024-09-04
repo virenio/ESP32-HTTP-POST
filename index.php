7e907533e9c27c48e82b59e0f69096ae8044088b
virenio <pawarvirendra2004@yahoo.com>
2024-09-04 15:59:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,28 @@
-<?php
-// 
-include_once('dataAccess.php');
- 
-$reading_Id   = "";
-
-if (isset($_POST['getData'])) {
-		
-	$reading = getReading();
-	$reading_Id   = $reading["id"];
-
-	$reading_Id= test_input($reading_Id);
-	echo $reading_Id;
-}
-else {
-	echo "No data posted with HTTP xxx POST.";
-}
-function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-}
-?>
+   <!--      
+  -->
+  <?php
+   include_once('dataAccess.php');      
+      
+      $reading = getReadings();       
+      $reading_val1 = $reading["val1"];
+      $reading_val2 = $reading["val2"];        
+  ?>
+  
+  <!DOCTYPE html>
   <html>
       <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
           
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       </head>
       <header class="header">
-          <h1> Data Display</h1>           
+          <h1> Data Display</h1>
+           
       </header>
   <body>
-             <p> Value 1 :<?php echo $reading_Id?></p>             
+             <p> Value 1 :<?php echo $reading_val1?></p>
+             <p> Value 2 :<?php echo $reading_val2?></p>
+   
+
   </body>
   </html>
  
