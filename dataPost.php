af4f90700c8fe518a8b3eda8c84d565d2ea95502
virenio <pawarvirendra2004@yahoo.com>
2024-09-04 16:05:25 +0530

    Update dataPost.php

--- dataPost.php
+++ dataPost.php
@@ -1,1 +1,24 @@
-
+<!--
+-->
+<?php
+  include_once('dataAccess.php');
+ 
+   $temp = $humi =   "";
+  $lim = 10;
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+      $val1 = test_input($_POST["val1"]);
+      $val2 = test_input($_POST["val2"]);
+ 
+      $result = insertReading( $val1, $val2 );    
+  }
+  else {
+    echo "No data posted with HTTP POST.";
+  }
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+ 
